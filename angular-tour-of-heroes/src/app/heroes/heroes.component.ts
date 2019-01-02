83c9727840686f2e1488a6c1fe9681dcb65a28d7
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-01-02 13:15:35 +0100

    Create heroes.component.ts

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  //  the component's CSS/HTML element selector
+  selector: 'app-heroes',
+  // the location of the component's template file
+  templateUrl: './heroes.component.html',
+  //  the location of the component's private CSS styles.
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  // hero = 'Windstorm';
+
+  // component property, from mock-heroes.ts
+  heroes = HEROES;
+
+  //  Refactoring the Hero class/type
+  selectedHero: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // Add the click event handler
+  onSelect(hero: Hero): void {
+    //  void: the absence of having any type at all. You may commonly see this as the return type of functions that do not return a value.
+
+    this.selectedHero = hero;
+  }
+
+}
+
+
