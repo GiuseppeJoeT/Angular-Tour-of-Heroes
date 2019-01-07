375fb4166e311ec7373c8333d173dab01e4bb2ef
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-01-07 17:49:08 +0100

    Update hero.service.ts

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,13 +2,18 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
+// This marks the class as one that participates in the dependency injection system.
 @Injectable({
+
+  // A provider is something that can create or deliver a service;
+  // in this case, it instantiates the HeroService class to provide the service.
   providedIn: 'root'
 })
 export class HeroService {
 
   constructor() { }
 
+  // getHeroes method returns the mock heroes
   getHeroes(): Hero[] {
     return HEROES;
   }
