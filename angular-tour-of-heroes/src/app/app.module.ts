0e4145da0af57d8d67e2a152f17ed5796d43016a
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-01-10 09:37:32 +0100

    MessagesComponent

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
