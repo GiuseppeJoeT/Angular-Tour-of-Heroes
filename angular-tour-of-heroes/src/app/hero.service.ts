2f7a39644985bf1c036276e9a65c01c607474183
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-01-11 17:29:50 +0100

    Update hero.service.ts

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of, from } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 // This marks the class as one that participates in the dependency injection system.
@@ -11,6 +11,7 @@ import { MessageService } from './message.service';
   // in this case, it instantiates the HeroService class to provide the service.
   providedIn: 'root'
 })
+
 export class HeroService {
 
   // Angular will inject the singleton MessageService into that property when it creates the HeroService.
@@ -19,6 +20,7 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     // TO DO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+
+      return of(HEROES);
   }
 }
