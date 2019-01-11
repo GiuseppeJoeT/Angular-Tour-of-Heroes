fca5a9c1e773c32bc74f46d7d1a36a48680fc6e4
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-01-11 17:28:46 +0100

    Refactoring HeroesComponent
    ngFor directive with an hyperlink tag (<a>) and the routerLink attribute

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -11,34 +11,25 @@ import { HeroService } from '../hero.service';
   //  the location of the component's private CSS styles.
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  // hero = 'Windstorm';
 
   // component property, from mock-heroes.ts
   // heroes = HEROES; refactoring after adding HeroService
   heroes: Hero[];
 
-  //  When the app starts, the selectedHero is undefined by design
-  selectedHero: Hero;
-
   //  singleton instance of HeroService
   constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    // Observable.subscribe()
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  // Add the click event handler
-  onSelect(hero: Hero): void {
-    //  void: the absence of having any type at all. You may commonly see this as the return type of functions that do not return a value.
-
-    this.selectedHero = hero;
+  getHeroes(): void {
+    // Observable.subscribe() represents the execution of an Observable
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
+
 
 }
 
