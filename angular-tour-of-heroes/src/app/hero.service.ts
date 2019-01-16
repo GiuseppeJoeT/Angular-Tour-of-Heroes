f6230d75c964d8e66ec3e6b5f475ad47942fafa8
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-01-16 12:00:53 +0100

    HttpClient and handleError

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,32 +1,73 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+
 
 // This marks the class as one that participates in the dependency injection system.
 @Injectable({
 
   // A provider is something that can create or deliver a service;
-  // in this case, it instantiates the HeroService class to provide the service.
+  // in this case, it instantiates the HeroService class to provide the service at root level
   providedIn: 'root'
 })
 
 export class HeroService {
 
+  private heroesUrl  = 'api/heroes';  // URL to web api
+
   // Angular will inject the singleton MessageService into that property when it creates the HeroService.
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
+  // return an array of mock heroes as an Observable
   getHeroes(): Observable<Hero[]> {
-    // TO DO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
 
-      return of(HEROES);
+    // TO DO: send the message _after_ fetching the heroes
+    // this.messageService.add('HeroService: fetched heroes');
+    // return of(HEROES);
+
+    /** GET heroes from the server */
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
+
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+       // TODO: send the error to remote logging infrastructure
+       console.error(error); // log to console instead
+
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
