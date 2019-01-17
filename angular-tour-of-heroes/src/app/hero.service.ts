f264547d4858f598197c08f77b7b25e12227d703
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-01-17 18:07:54 +0100

    POST()  and searchHeroes() methods

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
@@ -12,33 +13,45 @@ const httpOptions = {
   })
 };
 
-
 // This marks the class as one that participates in the dependency injection system.
-@Injectable({
-
-  // A provider is something that can create or deliver a service;
-  // in this case, it instantiates the HeroService class to provide the service at root level
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 
 export class HeroService {
 
   private heroesUrl  = 'api/heroes';  // URL to web api
 
-  // Angular will inject the singleton MessageService into that property when it creates the HeroService.
   constructor(
     private http: HttpClient,
+
+    // Angular will inject the singleton MessageService into that property when it creates the HeroService.
     private messageService: MessageService) { }
 
-  // return an array of mock heroes as an Observable
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
 
-    /** GET heroes from the server */
+    // return an array of mock heroes as an Observable
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
         tap(_ => this.log('fetched heroes')),
         catchError(this.handleError('getHeroes', []))
       );
+  }
+
+  /** GET hero by id. Return `undefined` when id not found */
+  getHeroNo404<Data>(id: number): Observable<Hero> {
+
+    const url = `${this.heroesUrl}/?id=${id}`;
+
+    return this.http.get<Hero[]>(url)
+      .pipe(
+        // Map — transform the items emitted by an Observable by applying a function to each item
+        map(heroes => heroes[0]), // returns a {0|1} element array
+          tap(h => {
+            const outcome = h ? 'fetched' : 'did not find';
+            this.log(`${outcome} hero id=${id}`);
+          }),
+          catchError(this.handleError<Hero>(`getHero id=${id}`)),
+        );
   }
 
   /** GET hero by id. Will 404 if id not found */
@@ -49,6 +62,32 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+   /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+
+    // The trim() method removes whitespace from both ends of a string.
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
+
+  //////// Save methods //////////
+
+   /** POST: add a new hero to the server */
+   addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
     );
   }
 
