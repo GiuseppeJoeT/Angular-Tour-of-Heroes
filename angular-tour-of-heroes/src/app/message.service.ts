94c0dce60e071b6487b2623e84418bff422d41de
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-01-24 14:57:34 +0100

    Update message.service.ts

--- angular-tour-of-heroes/src/app/message.service.ts
+++ angular-tour-of-heroes/src/app/message.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
+  // A provider is something that can create or deliver a service;
+  // in this case, it instantiates the HeroService class to provide the service at root level
   providedIn: 'root'
 })
 
